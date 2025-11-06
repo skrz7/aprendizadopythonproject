e3a1be9640d018720e29b038aa000282375f65e5
skrz7 <skrz7.cs@gmail.com>
2025-11-06 12:13:54 -0300

    testando só

--- teste01.py
+++ teste01.py
@@ -2,11 +2,3 @@ nome=input('Qual seu nome?')
 idade=input('Qual sua idade?')
 peso=input('Qual seu peso?')
 print(nome,idade,peso)
-
-nome=input('Qual seu nome?')
-print('Seu nome é', nome,'?')
-certeza=input('Tem certeza?')
-
-nome=input('Qual seu nome?')
-print('Seu nome é', nome,'?')
-certeza=input('Tem certeza?')
